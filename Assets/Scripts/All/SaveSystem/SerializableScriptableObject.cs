bd2551451f941faf272db40e9efc7e1de1ff4fba
vladv3 <vlad.vavilin73@gmail.com>
2022-09-22 22:14:15 +0300

    added files

---
+++ Assets/Scripts/All/SaveSystem/SerializableScriptableObject.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.Serialization;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class SerializableScriptableObject : ScriptableObject
+{
+    [FormerlySerializedAs("_guid")] [SerializeField, HideInInspector] private string m_guid;
+    public string Guid => m_guid;
+
+#if UNITY_EDITOR
+    void OnValidate()
+    {
+        var path = AssetDatabase.GetAssetPath(this);
+        m_guid = AssetDatabase.AssetPathToGUID(path);
+    }
+#endif
+}
