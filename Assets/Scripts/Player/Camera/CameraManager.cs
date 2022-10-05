79714e5bdaf1c5d13c3308bd38ce8c0de8895e71
what1slove <what1slove03@yandex.ru>
2022-10-05 23:31:16 +0300

    camera manager After loading scene - attach to player

--- Assets/Scripts/Player/Camera/CameraManager.cs
+++ Assets/Scripts/Player/Camera/CameraManager.cs
@@ -4,44 +4,50 @@ using Cinemachine;
 using Cinemachine.Editor;
 using UnityEngine;
 
-namespace Camera
+public class CameraManager : MonoBehaviour
 {
-    public class CameraManager : MonoBehaviour
+    [SerializeField] private Camera m_mainCamera = default;
+
+    // player camera
+    [SerializeField] private CinemachineVirtualCamera m_virtualCamera = default;
+
+    [SerializeField] private VoidEventChannelSO m_cameraShakeChannel = default;
+    [SerializeField] private TransformEventChannel m_targetChannel = default;
+
+    [SerializeField] private CinemachineImpulseSource m_impulseSource;
+
+    // [SerializeField] 
+    // TODO: ...
+    [SerializeField] private Transform m_target = default;
+
+
+    #region enable-disable
+
+    private void OnEnable()
     {
-        [SerializeField] private VoidEventChannelSO m_cameraShakeChannel = default;
-        [SerializeField] private TransformEventChannel m_targetChannel = default;
-        [SerializeField] private CinemachineImpulseSource m_impulseSource;
-        // [SerializeField] 
-        // TODO: ...
-        [SerializeField] private Transform m_target = default;
+        m_targetChannel.OnEventRaised += SetTarget;
+        // m_cameraShakeChannel.OnEventRaised += CameraShake;
+    }
+
+    private void OnDisable()
+    {
+        m_targetChannel.OnEventRaised -= SetTarget;
+        // m_cameraShakeChannel.OnEventRaised -= CameraShake;
+    }
+
+    #endregion
+
+    private void SetTarget(Transform target)
+    {
+        m_target = target;
+        m_virtualCamera.Follow = target;
+        m_virtualCamera.LookAt = target;
+    }
 
 
-        #region enable-disable
-
-        private void OnEnable()
-        {
-            m_targetChannel.OnEventRaised += SetTarget;
-            m_cameraShakeChannel.OnEventRaised += CameraShake;
-        }
-
-        private void OnDisable()
-        {
-            m_targetChannel.OnEventRaised -= SetTarget;
-            m_cameraShakeChannel.OnEventRaised -= CameraShake;
-        }
-
-        #endregion
-
-        private void SetTarget(Transform target)
-        {
-            m_target = target;
-        }
-
-
-        private void CameraShake()
-        {
-            // TODO: implement me
-            throw new NotImplementedException();
-        }
+    private void CameraShake()
+    {
+        // TODO: implement me
+        throw new NotImplementedException();
     }
 }
