784f6a089aa9bb71f85b0f38303e0f90b57c1df4
Vladislav <45938449+Lolik232@users.noreply.github.com>
2022-11-17 08:58:57 +0300

    Bug fix
    1.  fixed a bug with  set settings calling events in awake
    
    2. adding coroutines for loading saved settings

--- Assets/Scripts/Systems/SaveSystem/Settings/SettingsSystem.cs
+++ Assets/Scripts/Systems/SaveSystem/Settings/SettingsSystem.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using All.Events;
 using Systems.SaveSystem.Settings.ScriptableObjects;
 using UnityEngine;
@@ -10,7 +12,7 @@ namespace Systems.SaveSystem.Settings
         [SerializeField] private VoidEventChannelSO _saveSettingsEventChannelSO = default;
         [SerializeField] private SaveSystem         _saveSystem                 = default;
         [SerializeField] private SettingsSO         _currentSettings            = default;
-        
+
         [SerializeField] private FloatEventChannelSO _masterVolumeEventChannelSO  = default;
         [SerializeField] private FloatEventChannelSO _musicVolumeEventChannelSO   = default;
         [SerializeField] private FloatEventChannelSO _effectsVolumeEventChannelSO = default;
@@ -18,7 +20,6 @@ namespace Systems.SaveSystem.Settings
         private void Awake()
         {
             _saveSystem.LoadSettingsFromDisk();
-            SetSettings();
         }
 
         private void OnEnable()
@@ -26,21 +27,33 @@ namespace Systems.SaveSystem.Settings
             _saveSettingsEventChannelSO.OnEventRaised += SaveSettings;
         }
 
+        private void Start()
+        {
+            StartCoroutine(SetSettings());
+        }
+
         private void OnDisable()
         {
             _saveSettingsEventChannelSO.OnEventRaised -= SaveSettings;
         }
 
-        private void SetSettings()
+        private IEnumerator SetSettings()
         {
             _masterVolumeEventChannelSO.RaiseEvent(_currentSettings.masterVolume);
             _musicVolumeEventChannelSO.RaiseEvent(_currentSettings.musicVolume);
             _effectsVolumeEventChannelSO.RaiseEvent(_currentSettings.effectsVolume);
+            yield return null;
         }
 
         private void SaveSettings()
         {
+            StartCoroutine(SaveSettingsCoroutine());
+        }
+
+        private IEnumerator SaveSettingsCoroutine()
+        {
             _saveSystem.SaveSettingsOnDisk();
+            yield return null;
         }
     }
 }
